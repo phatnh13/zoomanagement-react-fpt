fab4213e90572eacb7d94c98f0ab2ce80a03e30b
PhongDinh03 <phongdvse171245@fpt.edu.vn>
2023-10-21 07:31:23 +0700

    fix

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,8 @@
 import React from "react";
 import Welcome from "./WelcomePage/Welcome";
 import HomeView from "./HomeView/HomeView";
-<<<<<<< HEAD
 import WelcomeMap from "./HomeMap/WelcomeMap";
 import WelcomeNews from "./HomeNews/WelcomeNews";
-=======
-import HomeMenu from "./HomeMenu/HomeMenu";
->>>>>>> 062cb4d916d5065213397a8b29bbb7cd21051756
 const Home = () => {
     return (
         <>
@@ -16,7 +12,6 @@ const Home = () => {
             <HomeView />
             </div>
             <Welcome />
-<<<<<<< HEAD
             <div style={{
                 backgroundColor: '#F7F1DB',
             }}>
@@ -24,9 +19,6 @@ const Home = () => {
 
             <WelcomeNews />
         </div >
-=======
-            <HomeMenu />
->>>>>>> 062cb4d916d5065213397a8b29bbb7cd21051756
         </>
     );
 };
