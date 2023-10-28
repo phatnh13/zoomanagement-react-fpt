66149d6fdd3ab82e1d43b9a795a8884ebd7655a4
PhongDinh03 <phongdvse171245@fpt.edu.vn>
2023-10-28 07:25:42 +0700

    lol

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -9,8 +9,12 @@ const News = () => {
     const [news, setNews] = useState([]);
 
     useEffect(() => {
-        fetch(`https://localhost:7193/api/News`)
-            .then(response => response.json())
+        fetch(`https://localhost:7193/api/News`, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json; charset=UTF-8"
+            }
+        }).then(response => response.json())
             .then(data => setNews(data.item));
     }, []);
 
@@ -24,11 +28,11 @@ const News = () => {
                             <div className="g-container">
                                 <div className="g-void">
                                     <section className="ce list frame frame-default frame-type-list frame-layout-0 list-type-news_pi1">
-                                        <div className="g-col-12 g-col-sm-12 g-col-md-12 g-col-lg-12 g-col-xl-12">
-                                            <div className="g-col-12 g-col-sm-12 g-col-md-12 g-col-lg-12 g-col-xl-12">
+                                        <Col>
+                                            <Col >
                                                 <div className="{item.Content}" itemScope="itemscope" itemType="http://schema.org/{item.Content}">
                                                     <div className="g-void">
-                                                        <div className="g-col-12 g-col-sm-12 g-col-md-8 g-off-md-2 g-col-lg-8 g-off-lg-2 g-col-xl-8 g-off-xl-2">
+                                                        <Col>
                                                             <div class=" news-details__meta">
                                                                 {/* <!-- date --> */}
                                                                 <span class="news-list-date">
@@ -77,20 +81,18 @@ const News = () => {
                                                                     </ul>
                                                                 </p>
                                                             </div>
-                                                            <div className="g-col-12 g-col-sm-12 g-col-md-8 g-off-md-2 g-col-lg-8 g-off-lg-2 g-col-xl-8 g-off-xl-2 u-break-grid__nested-container"></div>
-                                                            <div className="g-col-12 g-col-sm-12 g-col-md-8 g-off-md-2 g-col-lg-8 g-off-lg-2 g-col-xl-8 g-off-xl-2"></div>
-                                                            <div className="news-details__spacer"></div>
+
                                                             {/* link back */}
                                                             <div>
                                                                 <Link to="/allnews" className="btn btn--arrow iconfont--link-arrow iconfont__after ">
                                                                     Back {'>'}
                                                                 </Link>
                                                             </div>
-                                                        </div>
+                                                        </Col>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </div>
+                                            </Col>
+                                        </Col>
                                     </section>
                                 </div>
                             </div>
