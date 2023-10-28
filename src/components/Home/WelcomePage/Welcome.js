f4770eeabf5588bd3a25a2eb4e984f10584be2ec
TinKOA <phatnguyenhoang69@gmail.com>
2023-10-28 09:09:40 +0700

    finish updating WelcomeMap

--- src/components/Home/WelcomePage/Welcome.js
+++ src/components/Home/WelcomePage/Welcome.js
@@ -9,8 +9,6 @@ const Welcome = () => {
     return (
         <>
             <div className="welcome-text__top">
-                <div className="welcome-text__information">Welcome to Saigon Zoo</div>
-                <div className="about-zoo__information">Saigon Zoo is one of the buildings with the oldest history in Ho Chi Minh. Saigon Zoo is currently home to 590 animals of 125 species, 1,800 plants of 260 species; 23 species of domestic orchids; 33 species of cacti, 34 species of ornamental plants, green grass, etc.</div>
                 <div style={{ backgroundColor: '#8b9472' }}>
                     <div className="d-flex justify-content-end">
                         <Button className="btn-view-all-price mt-3 me-5 btn btn-success"><Link className="link-underline-opacity-0 link-underline link-light" to="/buyingticket">View all price</Link></Button>
