e5541e9a5c7c2cd1243a8a47f7681fc88c3f37e9
TinKOA <phatnguyenhoang69@gmail.com>
2023-11-05 20:16:12 +0700

    Update Navbar

--- src/components/Layout/DefaultLayout/Header/Header.js
+++ src/components/Layout/DefaultLayout/Header/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import {Navbar, Container, Nav, NavDropdown} from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Navbar.Brand>
+          <Link to='/'>Home</Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
