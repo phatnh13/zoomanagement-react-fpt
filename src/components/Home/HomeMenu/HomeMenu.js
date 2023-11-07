f7467c236a694e99499859bcee6e2d487429313a
TinKOA <phatnguyenhoang69@gmail.com>
2023-11-07 19:13:59 +0700

    Update Home Menu

--- src/components/Home/HomeMenu/HomeMenu.js
+++ src/components/Home/HomeMenu/HomeMenu.js
@@ -1,4 +1,4 @@
-import { Col } from 'react-bootstrap';
+import { Col, Button } from 'react-bootstrap';
 import "./HomeMenu.css"
 import { Link } from 'react-router-dom';
 const HomeMenu = ({handleClose}) => {
@@ -94,9 +94,22 @@ const HomeMenu = ({handleClose}) => {
         <Col className='navigation-list__right col-md-2'>
           <ul>
             <li>
+              
+              {userName === "" ? (
               <Link className='navigation__link' to='/login' onClick={handleClose}>
-              {userName === "" ? (<h1 className='navigation__title'>Login</h1>) : (<h1 className='navigation__title'>{userName}</h1>)}
+              <h1 className='navigation__title'>Login</h1>
               </Link>
+              ) : (
+              <>
+              <Link className='navigation__link' to='/user/profile' onClick={handleClose}>
+              <h1 className='navigation__title'>{userName}</h1>
+              </Link>
+              <Link className='navigation__link' to='/login'>
+              <h1 style={{fontWeight: 'normal', fontSize: '1rem'}} className='navigation__title'>Return to management</h1>
+              </Link>
+              </>
+              )}
+        
             </li>
           </ul>
         </Col>
