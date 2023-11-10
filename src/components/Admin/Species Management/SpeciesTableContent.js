5cb9464d7d3478e98b58438be720fb1e4c7292ef
Phan Nguyễn Hoàng Phương <phuongpnhse171239@fpt.edu.vn>
2023-11-10 21:01:30 +0700

    Add image to species table and revenue table to ticket management

--- src/components/Admin/Species Management/SpeciesTableContent.js
+++ src/components/Admin/Species Management/SpeciesTableContent.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { Accordion, Button } from "react-bootstrap";
+import { Accordion, Button, Image } from "react-bootstrap";
 import SpeciesDeleteModal from "./SpeciesDeleteModal";
 import { useNavigate } from "react-router-dom";
 
@@ -46,7 +46,8 @@ function SpeciesTableContent({ species, index, reloadState }) {
                 <p><strong>Ecological: </strong> {species.ecological}</p>
                 <p><strong>Diet: </strong> {species.diet}</p>
                 <p><strong>Breeding and reproduction: </strong> {species.breedingAndReproduction}</p>
-                <img src={species.image} alt="species" width="100%" height="auto" />
+                {/* <img src={species.image} alt="species" width="100%" height="auto" /> */}
+                <Image src={species.image} alt="species" width="300rem" height="300rem" />
                 <Button variant="danger" className="me-2" onClick={handleShow}>Delete</Button>
                 <Button variant="warning" onClick={handleUpdateSpecies}>Update</Button>
             </Accordion.Body>
